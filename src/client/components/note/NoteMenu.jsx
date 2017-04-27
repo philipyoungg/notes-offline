256ce8acdf6d5051d1adf01b4abf8c970ad1b485
Philip Young <philip.young.gunawan@gmail.com>
2017-04-27 12:34:30 +0700

    enable localstorage and pointer on button

--- src/client/components/note/NoteMenu.jsx
+++ src/client/components/note/NoteMenu.jsx
@@ -6,11 +6,11 @@ const NoteMenu = ({ handleClick }) => (
   <div className="flex justify-between items-center fixed w-100 h3 ph4 bb b--near-white">
     <p
       onClick={() => handleClick()}
-      className="f6 ba br2 b--black-30 pv2 ph3 mb0 dim"
+      className="f6 ba br2 b--black-30 pv2 ph3 mb0 dim pointer"
     > Add New Note </p>
     <p className='f6 o-70 mb0'>Works offline!</p>
-    
-    
+
+
   </div>
 );
 
